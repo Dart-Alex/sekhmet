5f4c5f2f730fb4711a9a6bedf0c91cbfd0ced017
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-03-12 19:47:28 +0100

    Auth automatic scaffolding

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,40 @@
+<nav class="navbar is-fixed-top" id="main-navbar">
+		<div class="container">
+			<div class="navbar-brand">
+				<a class="navbar-item" href="{{ url('/') }}">
+						{{ config('app.name', 'Laravel') }}
+				</a>
+				<a role="button" aria-label="menu" aria-expanded="false" data-target="navbarMenu" class="navbar-burger burger">
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+				</a>
+			</div>
+			<div class="navbar-menu" id="navbarMenu">
+				<div class="navbar-start">
+
+				</div>
+				<div class="navbar-end">
+					<!-- Authentication Links -->
+					@guest
+					<a class="navbar-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+
+					@if (Route::has('register'))
+					<a class="navbar-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+					@endif @else
+					<div class="navbar-item has-dropdown" data-target="userMenu">
+						<a id="navbarDropdown" href="#" class="navbar-link">{{ Auth::user()->name }} <span class="caret"></span></a>
+						<div class="navbar-dropdown is-boxed is-right" id="userMenu">
+							<a href="{{ route('logout') }}" class="navbar-item" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+								{{ __('Logout') }}
+							</a>
+							<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+									@csrf
+							</form>
+						</div>
+					</div>
+					@endguest
+				</div>
+			</div>
+		</div>
+	</nav>
