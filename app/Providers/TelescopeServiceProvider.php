3729a3578de19111c05e83d27643436fef7fd5aa
Dart-Alex <darkvcious@gmail.com>
2019-03-13 22:09:40 +0100

    Telescope only for admins

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -62,9 +62,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
 
-            ]);
+            return $user->isAdmin();
         });
     }
 }
