414b70d3efd42068a6ae3dab7660cf9a6b80e23f
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-03-14 14:27:13 +0100

    RouteKeyName

--- app/Chan.php
+++ app/Chan.php
@@ -6,35 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chan extends Model
 {
-    /**
+	/**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'description'
-    ];
+	protected $fillable = [
+		'name', 'description', 'hidden'
+	];
 
-    /**
+	/**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
+	protected $hidden = [];
 
-    ];
-
-    /**
+	/**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = [
+	protected $casts = [
 		'created_at' => 'datetime',
 		'updated_at' => 'datetime'
 	];
 
-	public function chanUsers() {
+	public function getRouteKeyName()
+	{
+		return 'name';
+	}
+	public function chanUsers()
+	{
 		return $this->hasMany(ChanUsers::class);
 	}
 }
