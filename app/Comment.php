4d8b9367695dfa5761399bb17ea1e4b78a785f6a
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-03-14 17:11:32 +0100

    Completed models

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'post_id', 'user_id', 'reply_to', 'name', 'content'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+		'created_at' => 'datetime',
+		'updated_at' => 'datetime'
+	];
+
+	public function post() {
+		return $this->belongsTo(Post::class);
+	}
+
+	public function replies() {
+		return $this->hasMany(Comment::class, 'reply_to', 'id');
+	}
+
+	public function user() {
+		return $this->belongsTo(User::class);
+	}
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'chan_id', 'name', 'content', 'date', 'comments_allowed'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+		'date' => 'datetime',
+		'created_at' => 'datetime',
+		'updated_at' => 'datetime'
+	];
+
+	public function chan() {
+		return $this->belongsTo(Chan::class, 'chan_id', 'id');
+	}
+
+	public function comments() {
+		return $this->hasMany(Comment::class);
+	}
 }
