78213b77e9149208b7470d996bf418540bd0067e
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-03-17 20:50:58 +0100

    Disabling localhost middleware for now

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
             'bindings',
 		],
 		'bot' => [
-			\App\Bot\Middleware\Localhost::class,
+			// \App\Bot\Middleware\Localhost::class,
 			\Illuminate\Routing\Middleware\SubstituteBindings::class,
 		],
     ];
