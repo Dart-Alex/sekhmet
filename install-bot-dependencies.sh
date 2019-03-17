12ac61399d6657b4fa0d62f45098b3fcd72892aa
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-03-17 20:36:59 +0100

    Newer library for irc

--- install-bot-dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script to download dependencies for sekhmet.py
-echo 'Downloading python dependencies'
-php -r "copy('https://raw.githubusercontent.com/jbalogh/python-irclib/master/ircbot.py', 'bot/ircbot.py');"
-php -r "copy('https://raw.githubusercontent.com/jbalogh/python-irclib/master/irclib.py', 'bot/irclib.py');"
-echo 'Downloading complete'
