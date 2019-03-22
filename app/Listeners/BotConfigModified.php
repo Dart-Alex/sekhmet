ea2fbbc926fd49bb7d78a8c07970c9a1870fb87a
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-03-22 11:49:36 +0100

    Watching for admin removal

--- app/Listeners/BotConfigModified.php
+++ app/Listeners/BotConfigModified.php
@@ -6,6 +6,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Cache;
 use Carbon\Carbon;
+use App\ChanUser;
 
 class BotConfigModified
 {
@@ -31,12 +32,30 @@ class BotConfigModified
 		return true;
 	}
 
+	public function chanUserDeleting($event)
+	{
+		if($event->chanUser->admin) {
+			Cache::put('bot-config-last-update', Carbon::now());
+		}
+		return true;
+	}
+
+	public function userDeleting($event)
+	{
+		if($event->user->isAdmin() || ChanUser::where('user_id', $event->user->id)->where('admin', true)->exists()) {
+			Cache::put('bot-config-last-update', Carbon::now());
+		}
+		return true;
+	}
+
 	public function subscribe($events)
 	{
 		$events->listen('App\Events\ChanAdded', 'App\Listeners\BotConfigModified');
 		$events->listen('App\Events\ChanDeleted', 'App\Listeners\BotConfigModified');
 		$events->listen('App\Events\ChanUpdated', 'App\Listeners\BotConfigModified');
 		$events->listen('App\Events\UserAdminSet', 'App\Listeners\BotConfigModified');
+		$events->listen('App\Events\UserDeleting', 'App\Listeners\BotConfigModified@userDeleting');
 		$events->listen('App\Events\ChanUserAdminSet', 'App\Listeners\BotConfigModified');
+		$events->listen('App\Events\ChanUserDeleting', 'App\Listeners\BotConfigModified@chanUserDeleting');
 	}
 }
