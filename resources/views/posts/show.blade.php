0ec6d80de20f9c87db2c75a3797df315400e38be
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-03-22 16:07:09 +0100

    Preparing for tinymce

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,7 @@
 <div class="content">
 	<h1>{{$post->name}}</h1>
 	<p>Aura lieu {{ $post->date->diffForHumans() }} ({{$post->date->isoFormat('LLLL')}})</p>
-	<p>{{$post->content}}</p>
+	<p>{!! $post->content !!}</p>
 	<comments id="{{$post->id}}"></comments>
 </div>
 @endsection
