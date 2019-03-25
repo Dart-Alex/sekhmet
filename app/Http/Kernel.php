967652cfc39de2228c12406d8a60f16b44a4df50
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-03-25 12:38:02 +0100

    Reactivating localhost middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,7 +48,7 @@ class Kernel extends HttpKernel
 			\App\Http\Middleware\EnforceJson::class,
 		],
 		'bot' => [
-			// \App\Bot\Middleware\Localhost::class,
+			\App\Bot\Middleware\Localhost::class,
 			\App\Bot\Middleware\BotAlive::class,
 			\Illuminate\Routing\Middleware\SubstituteBindings::class,
 			\App\Http\Middleware\EnforceJson::class,
