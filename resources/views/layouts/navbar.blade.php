9a1b0ccfb17c0dd18270991d1046246c8cd6c71d
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-03-28 15:50:24 +0100

    Add chan list to navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -12,7 +12,18 @@
 			</div>
 			<div class="navbar-menu" id="navbarMenu">
 				<div class="navbar-start">
-
+					<div class="navbar-item has-dropdown" data-target="chanMenu">
+						<a href="#" class="navbar-link" id="chanDropdown">Chans</a>
+						<div class="navbar-dropdown is-boxed" id="chanMenu">
+							<?php
+								if(!auth()->guest() && auth()->user()->isAdmin()) $chansMenu = \App\Chan::orderBy('name', 'ASC')->get();
+								else $chansMenu = \App\Chan::orderBy('name', 'ASC')->where('hidden', false)->get();
+							?>
+							@foreach ($chansMenu as $chan)
+							<a href="{{ route('chans.show', ['chan' => $chan->name]) }}" class="navbar-item">{{$chan->displayName()}}</a>
+							@endforeach
+						</div>
+					</div>
 				</div>
 				<div class="navbar-end">
 					<!-- Authentication Links -->
@@ -23,7 +34,7 @@
 					<a class="navbar-item" href="{{ route('register') }}">{{ __('Register') }}</a>
 					@endif @else
 					<div class="navbar-item has-dropdown" data-target="userMenu">
-						<a id="navbarDropdown" href="#" class="navbar-link">{{ Auth::user()->name }} <span class="caret"></span></a>
+						<a id="userDropdown" href="#" class="navbar-link">{{ Auth::user()->name }} <span class="caret"></span></a>
 						<div class="navbar-dropdown is-boxed is-right" id="userMenu">
 							<a href="{{ route('logout') }}" class="navbar-item" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
 								{{ __('Logout') }}
