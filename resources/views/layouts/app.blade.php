f60f5143201c2d78be3312e0f7ded1bf457d9d20
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-03-30 15:44:37 +0100

    Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,16 @@
 		<main class="container">
 			@yield('content')
 		</main>
+		<footer class="footer">
+			<div class="content has-text-centered">
+				<a href="{{route('polconf')}}">Politique de confidentialité.</a><br/>
+
+				Site développé par <a href="https://github.com/Dart-Alex/" target="_blank">Dart-Alex</a>.<br/>
+				<a href="https://bulma.io/made-with-bulma/" target="_blank">
+					<img src="https://bulma.io/images/made-with-bulma.png" alt="Made with Bulma" width="128" height="24">
+				</a>
+			</div>
+		</footer>
 		<!-- Scripts -->
 		@if(session('message'))
 		<message type="{{ session('message')["type"] }}" content="{{ session('message')["content"] }}"></message>
--- resources/views/youtubeVideos/index.blade.php
+++ resources/views/youtubeVideos/index.blade.php
@@ -1,55 +1,57 @@
 @extends('layouts.app')
 @section('content')
-<div class="content">
-	<form action="{{route('youtubeVideos.search', ['chan' => $chan->name])}}" method="post">
-		@csrf
-	<div class='field is-grouped'>
-		<div class='control'>
-			<input class='input' type='text' name='name' id='name' placeholder='Recherche' value='{{old('name')}}' required/>
-		</div>
-		<div class="control">
-			<input type="submit" class="button is-primary" value="Rechercher"/>
-		</div>
-		@if ($errors->has('name'))
-		<p class='help is-danger'>
-			{{ $errors->first('name') }}
-		</p>
-		@endif
-	</div>
-	</form>
-	<table class='table-responsive'>
-		<thead>
-			<tr>
-				<th>Index</th>
-				<th>Vidéo</th>
-				<th>Titre</th>
-				<th>Par</th>
-				<th>Le</th>
-			</tr>
-		</thead>
-		<tbody>
-		@foreach ($youtubeVideos as $youtubeVideo)
-			<?php $info = $youtubeVideo->getInfo() ?>
-			@if($info)
-			<tr>
-				<td>{{$youtubeVideo->getIndex()}}</td>
-				<td>
-					<a href="https://youtu.be/{{$youtubeVideo->yid}}" target="__blank">
-						<img src="{{$info->snippet->thumbnails->default->url}}" height="{{$info->snippet->thumbnails->default->height}}" width="{{$info->snippet->thumbnails->default->width}}"/>
-					</a>
-				</td>
-				<td>
-					<a href="https://youtu.be/{{$youtubeVideo->yid}}" target="__blank">
-						{{$info->snippet->title}} [{{$youtubeVideo->getDuration()}}]
-					</a>
-				</td>
-				<td><a href={{route('youtubeVideos.indexName', ['chan' => $chan, 'name' => $youtubeVideo->displayName()])}}>{{$youtubeVideo->displayName()}}</a></td>
-				<td>{{$youtubeVideo->created_at->isoFormat('LLLL')}}</td>
-			</tr>
+<div class="container">
+	<div class="content">
+		<form action="{{route('youtubeVideos.search', ['chan' => $chan->name])}}" method="post">
+			@csrf
+		<div class='field is-grouped'>
+			<div class='control'>
+				<input class='input' type='text' name='name' id='name' placeholder='Recherche' value='{{old('name')}}' required/>
+			</div>
+			<div class="control">
+				<input type="submit" class="button is-primary" value="Rechercher"/>
+			</div>
+			@if ($errors->has('name'))
+			<p class='help is-danger'>
+				{{ $errors->first('name') }}
+			</p>
 			@endif
-		@endforeach
-		</tbody>
-	</table>
+		</div>
+		</form>
+		<table class='table-responsive'>
+			<thead>
+				<tr>
+					<th>Index</th>
+					<th>Vidéo</th>
+					<th>Titre</th>
+					<th>Par</th>
+					<th>Le</th>
+				</tr>
+			</thead>
+			<tbody>
+			@foreach ($youtubeVideos as $youtubeVideo)
+				<?php $info = $youtubeVideo->getInfo() ?>
+				@if($info)
+				<tr>
+					<td>{{$youtubeVideo->getIndex()}}</td>
+					<td>
+						<a href="https://youtu.be/{{$youtubeVideo->yid}}" target="__blank">
+							<img src="{{$info->snippet->thumbnails->default->url}}" height="{{$info->snippet->thumbnails->default->height}}" width="{{$info->snippet->thumbnails->default->width}}"/>
+						</a>
+					</td>
+					<td>
+						<a href="https://youtu.be/{{$youtubeVideo->yid}}" target="__blank">
+							{{$info->snippet->title}} [{{$youtubeVideo->getDuration()}}]
+						</a>
+					</td>
+					<td><a href={{route('youtubeVideos.indexName', ['chan' => $chan, 'name' => $youtubeVideo->displayName()])}}>{{$youtubeVideo->displayName()}}</a></td>
+					<td>{{$youtubeVideo->created_at->isoFormat('LLLL')}}</td>
+				</tr>
+				@endif
+			@endforeach
+			</tbody>
+		</table>
+	</div>
 
 	{{$youtubeVideos->links()}}
 </div>
