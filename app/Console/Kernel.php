35ab6387904f67b4a2c23dd536a5a4988dcc9c6a
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-04-01 15:13:24 +0200

    scheduled commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+		$schedule->command('telescope:prune')->daily();
+		$schedule->command('cacheYoutube')->weekly();
         // $schedule->command('inspire')
         //          ->hourly();
     }
