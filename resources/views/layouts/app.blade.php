7cb1ee64501a2762df59f2e082a65ca0eb4a6702
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-04-01 11:26:04 +0200

    Added cookies message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,12 +31,37 @@
 
 	<!-- Favicon -->
 	<link rel="Shortcut Icon" href="{{ asset('favicon.ico')}}" type="image/x-icon"/>
+	<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+	<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+	<script>
+	window.addEventListener("load", function(){
+	window.cookieconsent.initialise({
+	"palette": {
+		"popup": {
+		"background": "#edeff5",
+		"text": "#838391"
+		},
+		"button": {
+		"background": "#4b81e8"
+		}
+	},
+	"position": "top",
+	"static": true,
+	"content": {
+		"message": "Ce site utilise des cookies pour vous offrir une expérience utilisateur de qualité. En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies.",
+		"dismiss": "Ok !",
+		"link": ""
+	}
+	})});
+	</script>
 </head>
 
 <body class="has-navbar-fixed-top">
 	<div id="app">
 	@include('layouts.navbar')
+	<div class="container">
 	{{Breadcrumbs::render()}}
+	</div>
 		<main class="container">
 			@yield('content')
 		</main>
