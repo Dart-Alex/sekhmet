0acce67bf55237c08cbf691247679d43d83a94cc
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-04-04 15:16:34 +0200

    New design

--- resources/views/chans/show.blade.php
+++ resources/views/chans/show.blade.php
@@ -1,64 +1,67 @@
 @extends('layouts.app')
 @section('content')
 <div class="content">
-
-	<h1>{{ $chan->displayName() }}</h1>
-	<p>{{ $chan->description }}</p>
-	<div class="buttons">
-		<a class="button is-info" href="{{route('posts.index', ['chan' => $chan->name])}}">
-			<span class="icon"><i class="fas fa-calendar-alt"></i></span>
-			<span>&Eacute;v&eacute;nements</span>
-		</a>
-		<a class="button is-info" href="{{route('youtubeVideos.index', ['chan' => $chan->name])}}">
-			<span class="icon"><i class="fab fa-youtube"></i></span>
-			<span>Youtube</span>
-		</a>
-		<a class="button is-info" href="{{route('contact')}}?chan={{$chan->name}}">
-			<span class="icon"><i class="fas fa-envelope"></i></span>
-			<span>Contacter les administrateurs</span>
-		</a>
-
-		@can('join', $chan)
-		<a class="button is-primary" onclick="event.preventDefault();document.getElementById('form-join').submit()">
-			Rejoindre le chan
-		</a>
-		<form id="form-join" action='{{route('chanUsers.store', ['chan' => $chan->name])}}' method='POST'>
-			@csrf
-		</form>
-		@endcan
-		@can('part', $chan)
-		<a class="button is-danger" onclick="event.preventDefault();document.getElementById('form-part').submit()">
-			Partir du chan
-		</a>
-		<form id="form-part" action="{{route('chanUsers.destroy', ['chan' => $chan->name, 'chanUser' => $chan->chanUser(auth()->user())])}}" method="post">
-			@csrf
-			@method('DELETE')
-		</form>
-		@endcan
-		@can('update', $chan)
-		<a href="{{route('chans.edit', ['chan' => $chan->name])}}" class="button is-warning">Modifier le chan</a>
-		@endcan
-		@can('delete', $chan)
-		<a class="button is-danger" onclick="event.preventDefault();document.getElementById('form-chan-delete').submit()">
-			Supprimer le chan
-		</a>
-		<form id="form-chan-delete" action="{{route('chans.destroy', ['chan' => $chan->name])}}" method="POST">
-			@csrf
-			@method('DELETE')
-		</form>
-		@endcan
+	<div class="card">
+		<header class="card-header">
+			<div class="card-header-title is-size-3">
+				{{ $chan->displayName() }}
+			</div>
+			@can('update', $chan)
+			<a href="{{route('chans.edit', ['chan' => $chan->name])}}" class="card-header-icon has-text-warning">
+				<span class="icon">
+					<i class="fas fa-edit"></i>
+				</span>
+			</a>
+			@endcan
+			@can('delete', $chan)
+			<a title="Supprimer" class="card-header-icon has-text-danger" onclick="event.preventDefault();document.getElementById('form-chan-delete').submit()">
+				<span class="icon">
+					<i class="fas fa-trash"></i>
+				</span>
+			</a>
+			<form id="form-chan-delete" action="{{route('chans.destroy', ['chan' => $chan->name])}}" method="POST">
+				@csrf
+				@method('DELETE')
+			</form>
+			@endcan
+		</header>
+		<div class="card-content">
+			{{ $chan->description }}
+		</div>
+		<footer class="card-footer">
+			<a title="Événements" class="card-footer-item" href="{{route('posts.index', ['chan' => $chan->name])}}">
+				<span class="icon"><i class="fas fa-calendar-alt"></i></span>
+				<span class="is-hidden-mobile">&Eacute;v&eacute;nements</span>
+				<span>({{$chan->countNewPosts()}})</span>
+			</a>
+			<a title="Youtube" class="card-footer-item" href="{{route('youtubeVideos.index', ['chan' => $chan->name])}}">
+				<span class="icon"><i class="fab fa-youtube"></i></span>
+				<span class="is-hidden-mobile">Youtube</span>
+				<span>({{$chan->countYoutubeVideos()}})</span>
+			</a>
+			<a title="Contact" class="card-footer-item" href="{{route('contact')}}?chan={{$chan->name}}">
+				<span class="icon"><i class="fas fa-envelope"></i></span>
+				<span class="is-hidden-mobile">Contact</span>
+			</a>
+		</footer>
 	</div>
-	@if($chan->chanUsers->count() > 0)
-	<h2>Utilisateurs</h2>
-	<ul class="fa-ul">
+
+	</div>
+	<div class="panel">
+		<p class="panel-heading">
+			Utilisateurs
+		</p>
+		@if($chan->chanUsers->count() > 0)
 		@foreach($chan->chanUsers->sortByDesc('admin') as $chanUser)
-		<li {!!($chanUser->admin?'style="list-style-type:none;"':'')!!}>
-			{!!$chanUser->admin?'<span class="fa-li"><i class="fas fa-at"></i></span>':''!!}
-			<span>
-				{{$chanUser->user->name}}
-				({{join(', ', $chanUser->user->ircNames->pluck('name')->toArray())}})
+		<div class="panel-block">
+			<span class="panel-icon">
+				@if($chanUser->admin)
+				<i class="fas fa-at"></i>
+				@endif
 			</span>
-			<span style="display:inline-flex;">
+			{{$chanUser->user->name}}
+			({{join(', ', $chanUser->user->ircNames->pluck('name')->toArray())}})
+			<span class="is-inline-flex is-pulled-right">
 				@can('update', [$chanUser, $chan])
 				<a title="{{($chanUser->admin?'Rendre utilisateur':'Rendre admin')}}" href="{{route('chanUsers.update', ['chan' => $chan->name, 'chanUser' => $chanUser->id])}}" onclick="event.preventDefault();document.getElementById('update-form-{{$chanUser->id}}').submit();">
 					<span class="icon"><i class="fas fa-chevron-{{($chanUser->admin?'down':'up')}}"></i></span>
@@ -78,10 +81,11 @@
 				</form>
 				@endcan
 			</span>
-		</li>
+		</div>
 		@endforeach
+		@endif
 		@can('update', $chan)
-		<li>
+		<div class="panel-block">
 			<form action='{{route('chanUsers.store', ['chan' => $chan->name])}}' method='post'>
 				@csrf
 				<div class="field is-grouped">
@@ -101,9 +105,29 @@
 					</div>
 				</div>
 			</form>
-		</li>
+		</div>
 		@endcan
-	</ul>
-	@endif
+		@can('join', $chan)
+		<div class="panel-block">
+			<a class="button is-primary is-outlined is-fullwidth" onclick="event.preventDefault();document.getElementById('form-join').submit()">
+				Rejoindre le chan
+			</a>
+			<form id="form-join" action='{{route('chanUsers.store', ['chan' => $chan->name])}}' method='POST'>
+				@csrf
+			</form>
+		</div>
+		@endcan
+		@can('part', $chan)
+		<div class="panel-block">
+			<a class="button is-danger is-outlined is-fullwidth" onclick="event.preventDefault();document.getElementById('form-part').submit()">
+				Partir du chan
+			</a>
+			<form id="form-part" action="{{route('chanUsers.destroy', ['chan' => $chan->name, 'chanUser' => $chan->chanUser(auth()->user())])}}" method="post">
+				@csrf
+				@method('DELETE')
+			</form>
+		</div>
+		@endcan
+	</div>
 </div>
 @endsection
