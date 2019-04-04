fb5a83ce7fd30ef8bab633bdc0d333226d95cf76
Alexandre Dartinet <alexandre.dartinet@gmail.com>
2019-04-04 14:16:43 +0200

    padding at the top of breadcrumbs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
 		<message type="{{ session('message')["type"] }}" content="{{ session('message')["content"] }}"></message>
 		@endif
 		@include('layouts.navbar')
-		<div class="container">
+		<div class="container" style="padding-top:20px;">
 			{{Breadcrumbs::render()}}
 		</div>
 		<main class="container">
